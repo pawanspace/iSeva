0381d753cf159e90ae649cde44803d37fd2a7920
Pawan <pawanspace@gmail.com>
2014-05-08 00:48:20 -0400

    Adding interface for Seva

---
+++ iSeva/SevaTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SevaTableViewController.h
+//  iSeva
+//
+//  Created by Pawan Chopra on 5/8/14.
+//  Copyright (c) 2014 RSSB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SevaTableViewController : UITableViewController
+
+@end
